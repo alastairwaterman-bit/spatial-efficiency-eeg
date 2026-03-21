d25c6a0dc4942784be18727baa2df2b234ea1f74
alastairwaterman-bit <alastairwaterman@gmail.com>
2026-03-21 08:27:33 +0000

    Update author name in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="spatial-efficiency-eeg",
     version="1.0.0",
-    author="Alastair Waterman",
+    author="[Author]",
     description="Spatial efficiency metrics for EEG consciousness research",
     packages=find_packages(),
     python_requires=">=3.8",
